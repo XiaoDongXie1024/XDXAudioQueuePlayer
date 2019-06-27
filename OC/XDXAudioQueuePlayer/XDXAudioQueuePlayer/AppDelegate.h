f9ca5ea168408e4a94a500dcbb81a0ce86f6f2ac
Demon <demonli@tvunetworks.com>
2019-06-27 14:57:38 +0800

    Init Project

---
+++ OC/XDXAudioQueuePlayer/XDXAudioQueuePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDXAudioQueuePlayer
+//
+//  Created by 小东邪 on 2019/6/27.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC/XDXAudioQueuePlayer/XDXAudioQueuePlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XDXAudioQueuePlayer
+//
+//  Created by 小东邪 on 2019/6/27.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
